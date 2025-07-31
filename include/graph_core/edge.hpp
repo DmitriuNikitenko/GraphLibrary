d4bee352c1cdd2ca316ee969162a902b2793697a
Dmitriu <ddimyc34@gmail.com>
2025-07-31 20:16:16 +0300

    feat: adding comparison operator, replacing pointers with smart pointers

--- include/graph_core/edge.hpp
+++ include/graph_core/edge.hpp
@@ -2,27 +2,37 @@
 #include "node.hpp"
 
 //We implement an adjacency list, where for each vertex (node) a list of its neighbors is stored
-template <typename T, typename WEIGHT = int>
+template <typename T, typename WEIGHT_TYPE = int>
 class Edge {
 private:
-	Node<T>* to_node;
-	WEIGHT weight = 0;
+	std::weak_ptr<Node<T>> to_node;
+	WEIGHT_TYPE weight = 0;
 public:
 	//Constructors and destructor
 	Edge() = delete;
-	Edge(Node<T>* _to_node, WEIGHT _weight = 0) : to_node(_to_node), weight(_weight) {}
+	Edge(std::shared_ptr<Node<T>> _to_node, WEIGHT_TYPE _weight = 0) : to_node(_to_node), weight(_weight) {}
 	~Edge() = default;
 
 	//Main functions
-	Node<T>* get_to_node() {
-		return to_node;
+	std::shared_ptr<Node<T>> get_to_node() const {
+		return to_node.lock();
 	}
 
-	const Node<T>* get_to_node() const {
-		return to_node;
+	void set_weight(WEIGHT_TYPE _weight) {
+		weight = _weight;
 	}
 
-	WEIGHT get_weight() const {
+	WEIGHT_TYPE get_weight() const {
 		return weight;
 	}
+
+	//Operator
+	bool operator==(const Edge<T, WEIGHT_TYPE>& other) const {
+		std::shared_ptr<Node<T>> this_to_node = to_node.lock();
+		std::shared_ptr<Node<T>> other_to_node = other.to_node.lock();
+
+		return this_to_node && other_to_node &&
+			this_to_node == other_to_node && weight == other.weight;
+	}
+
 };
